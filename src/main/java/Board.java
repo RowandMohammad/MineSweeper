f9306cc67ec27a3959062c3dbc1a20d9057de0d0
Rowand Mohammad <rowandnajim@gmail.com>
2023-08-22 11:44:10 +0100

    Added testing for board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -2,6 +2,16 @@ import java.util.ArrayList;
 
 class Board {
     private ArrayList<ArrayList<String>> board;
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
 
     Board(int sides) {
         board = new ArrayList<>();
@@ -22,12 +32,22 @@ class Board {
     }
 
     public void flagCell(int row, int col) {
-        if (board.get(row).get(col).equals("?")) {
-            board.get(row).set(col, "F");
-        } else if (board.get(row).get(col).equals("F")) {
-            board.get(row).set(col, "?");
+        if (isValid(row, col)) {
+            if (board.get(row).get(col).equals("?")) {
+                board.get(row).set(col, "F");
+            } else if (board.get(row).get(col).equals("F")) {
+                board.get(row).set(col, "?");
+            }
+        } else {
+            System.out.println("Invalid coordinates! Can't flag this cell.");
         }
     }
+
+    // Add this method to the Board class for bounds checking.
+    private boolean isValid(int row, int col) {
+        return row >= 0 && row < board.size() && col >= 0 && col < board.get(0).size();
+    }
+
 
     public boolean isFlagged(int row, int col) {
         return "F".equals(board.get(row).get(col));
@@ -52,29 +72,59 @@ class Board {
 
     public void printBoard() {
         System.out.println();
-        System.out.print("   ");
+
+        // Print header with column numbers
+        System.out.print("    "); // Initial spacing to align with row numbers
         for (int i = 0; i < board.size(); i++) {
             if (i < 10) {
-                System.out.print(" ");
+                System.out.print(ANSI_CYAN + " " + i + " " + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_CYAN + i + " " + ANSI_RESET);
             }
-            System.out.print(i + " ");
         }
         System.out.println();
+
         for (int i = 0; i < board.size(); i++) {
-            System.out.print(i + "  ");
+            // Print row number with proper spacing
             if (i < 10) {
-                System.out.print(" ");
+                System.out.print(ANSI_CYAN + i + "   " + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_CYAN + i + "  " + ANSI_RESET);
             }
             for (int j = 0; j < board.get(i).size(); j++) {
-                if ("9".equals(board.get(i).get(j))) {
-                    System.out.print("   ");
-                } else {
-                    System.out.print(board.get(i).get(j) + "  ");
+                String cellValue = board.get(i).get(j);
+                System.out.print("|");  // this creates the grid effect
+                switch (cellValue) {
+                    case "1":
+                        System.out.print(ANSI_BLUE + cellValue + " " + ANSI_RESET);
+                        break;
+                    case "2":
+                        System.out.print(ANSI_GREEN + cellValue + " " + ANSI_RESET);
+                        break;
+                    case "3":
+                        System.out.print(ANSI_YELLOW + cellValue + " " + ANSI_RESET);
+                        break;
+                    case "4":
+                    case "5":
+                    case "6":
+                        System.out.print(ANSI_RED + cellValue + " " + ANSI_RESET);
+                        break;
+                    case "F":
+                        System.out.print(ANSI_PURPLE + cellValue + " " + ANSI_RESET);
+                        break;
+                    case "*":
+                        System.out.print(ANSI_BLACK + cellValue + " " + ANSI_RESET);
+                        break;
+                    default:
+                        System.out.print(cellValue + " ");
                 }
             }
-            System.out.println();
+            System.out.println("|");  // this creates the grid effect for the last cell
         }
     }
+
+
+
 
     public int getSize() {
         return board.size();
