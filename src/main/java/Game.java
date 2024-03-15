907eb2309b949c073646f89d36a9e1477331a9a7
Rowand Mohammad <rowandnajim@gmail.com>
2024-03-15 12:00:00 +0000

    Implemented the main game logic for Minesweeper in the Game class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,10 +7,60 @@ class Game {
     private Mine mineManager;
     private int SIDES;
     private int MINES;
+    private int movesLeft;
     private Scanner in = new Scanner(System.in);
 
     Game() {
         chooseMode();
+        userBoard = new Board(SIDES);
+        realBoard = new Board(SIDES);
+        mineManager = new Mine(SIDES, MINES);
+        movesLeft = SIDES * SIDES;
+        initializeRealBoard();
+        placeCounts();
+    }
+
+    private void initializeRealBoard() {
+        for (ArrayList<Integer> mineLocation : mineManager.getMinesLocation()) {
+            realBoard.placeMine(mineLocation.get(0), mineLocation.get(1));
+        }
+    }
+
+    private boolean isValid(int row, int col) {
+        return row >= 0 && row < SIDES && col >= 0 && col < SIDES;
+    }
+
+    private void expand(int row, int col) {
+        if (!isValid(row, col)) return;
+        if (realBoard.isMine(row, col)) return;
+        if (!userBoard.getCell(row, col).equals("?")) return;
+
+        movesLeft--;
+
+        String data = realBoard.getCell(row, col);
+        if (!data.equals("0")) {
+            userBoard.setCell(row, col, data);
+        } else {
+            userBoard.setCell(row, col, " ");
+            for (int r = row - 1; r <= row + 1; r++) {
+                for (int c = col - 1; c <= col + 1; c++) {
+                    if (r != row || c != col) {
+                        expand(r, c);
+                    }
+                }
+            }
+        }
+    }
+
+    private void placeCounts() {
+        for (int i = 0; i < SIDES; i++) {
+            for (int j = 0; j < SIDES; j++) {
+                if (!realBoard.isMine(i, j)) {
+                    int count = mineManager.getAdjacentMinesCount(i, j);
+                    realBoard.setCell(i, j, Integer.toString(count));
+                }
+            }
+        }
     }
 
     private void chooseMode() {
@@ -36,24 +86,35 @@ class Game {
         }
     }
 
-    private void initializeRealBoard() {
-        for (ArrayList<Integer> mineLocation : mineManager.getMinesLocation()) {
-            realBoard.placeMine(mineLocation.get(0), mineLocation.get(1));
-        }
-    }
+    public void playMineSweeper() {
+        boolean gameOver = false;
+        while (!gameOver) {
+            if (movesLeft == MINES && !gameOver) {
+                userBoard.printBoard();
+                System.out.println("You Won!!!");
+                break;
+            }
 
-    private boolean isValid(int row, int col) {
-        return row >= 0 && row < SIDES && col >= 0 && col < SIDES;
-    }
+            System.out.println("Moves left:" + (movesLeft - MINES));
+            userBoard.printBoard();
+            System.out.println("Enter your move (row col):");
+            int myX = in.nextInt();
+            int myY = in.nextInt();
 
-
-
-    private void placeCounts() {
-        for (int i = 0; i < SIDES; i++) {
-            for (int j = 0; j < SIDES; j++) {
-                if (!realBoard.isMine(i, j)) {
-                    int count = mineManager.getAdjacentMinesCount(i, j);
-                    realBoard.setCell(i, j, Integer.toString(count));
+            if (userBoard.getCell(myX, myY).equals("?")) {
+                if (realBoard.isMine(myX, myY)) {
+                    gameOver = true;
+                    System.out.println("***Mines***");
+                    System.out.println("You Lost!!!");
+                    userBoard.setCell(myX, myY, "*");
+                    userBoard.printBoard();
+                    System.out.println("Press 0 to view all Mines");
+                    if (in.nextInt() == 0) {
+                        System.out.println("Solution");
+                        realBoard.printBoard();
+                    }
+                } else {
+                    expand(myX, myY);
                 }
             }
         }
