a8f9029d84fdb92c421db0713d4caad5d1df2e4e
Rowand Mohammad <rowandnajim@gmail.com>
2024-04-02 12:00:00 +0100

    Testing jacoco

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.playMineSweeper();
+        Game minesweeper = new Game();
+        minesweeper.playMineSweeper();
     }
 }
